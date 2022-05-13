4bd5f759b9986a84be9c05e60ad21e59fa8ecce9
jamiewri <jamiewri@me.com>
2022-05-13 10:33:20 +1000

    addinmg vs

---
+++ variable-sets.tf
@@ -0,0 +1,15 @@
+resource "tfe_variable_set" "dev" {
+  name         = "Dev Vars"
+  description  = "Variable set to be attached to each dev workspace"
+  global       = false
+  organization = var.organization
+}
+
+resource "tfe_variable" "dev" {
+  key             = "environement"
+  value           = "{ environment = 'dev'}"
+  hcl = true
+  category        = "terraform"
+  description     = "Sets the env"
+  variable_set_id = tfe_variable_set.dev.id
+}
